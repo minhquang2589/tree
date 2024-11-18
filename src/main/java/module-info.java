3dfbd1ef632ed8f1207e612c9d17afed7075bcef
minhquang2589 <ngominhquang724@gmail.com>
2024-11-18 16:51:14 +0700

    update

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,8 +2,8 @@ module com.example.demo {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
-    requires java.desktop;
     requires java.prefs;
+    requires javafx.graphics;
 
     opens com.example.demo to javafx.fxml;
     exports com.example.demo;
