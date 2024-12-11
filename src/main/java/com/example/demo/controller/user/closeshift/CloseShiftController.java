0f0c76e2c531ed88f2b0dc9f578df90a98e0545d
nguyenngocphong98 <166921042+nguyenngocphong98@users.noreply.github.com>
2024-12-11 21:36:15 +0700

    fixx bug kết ca
    fixx bug kết ca

--- src/main/java/com/example/demo/controller/user/closeshift/CloseShiftController.java
+++ src/main/java/com/example/demo/controller/user/closeshift/CloseShiftController.java
@@ -5,7 +5,6 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
-
 public class CloseShiftController {
 
     @FXML
@@ -40,21 +39,92 @@ public class CloseShiftController {
     private TextField textField14;
     @FXML
     private TextField textField15;
+    @FXML
+    private Button button0;
+    @FXML
+    private Button button1;
+    @FXML
+    private Button button2;
+    @FXML
+    private Button button3;
+    @FXML
+    private Button button4;
+    @FXML
+    private Button button5;
+    @FXML
+    private Button button6;
+    @FXML
+    private Button button7;
+    @FXML
+    private Button button8;
+    @FXML
+    private Button button9;
+    @FXML
+    private Button button00;
+    @FXML
+    private Button button000;
+    @FXML
+    private Button buttonxoa;
+    @FXML
+    private Button buttonc;
+    @FXML
+    private Button buttonnhap;
+    @FXML
+    private Button buttonok;
+
 
     private TextField selectedTextField;
 
+
+
+    @FXML
+    private void initialize() {
+        textField.setFocusTraversable(false);
+        textField1.setFocusTraversable(false);
+        textField2.setFocusTraversable(false);
+        textField3.setFocusTraversable(false);
+        textField4.setFocusTraversable(false);
+        textField5.setFocusTraversable(false);
+        textField6.setFocusTraversable(false);
+        textField7.setFocusTraversable(false);
+        textField8.setFocusTraversable(false);
+        textField9.setFocusTraversable(false);
+        textField10.setFocusTraversable(false);
+        textField11.setFocusTraversable(false);
+        textField12.setFocusTraversable(false);
+        textField13.setFocusTraversable(false);
+        textField14.setFocusTraversable(false);
+        textField15.setFocusTraversable(false);
+        button0.setFocusTraversable(false);
+        button1.setFocusTraversable(false);
+        button2.setFocusTraversable(false);
+        button3.setFocusTraversable(false);
+        button4.setFocusTraversable(false);
+        button5.setFocusTraversable(false);
+        button6.setFocusTraversable(false);
+        button7.setFocusTraversable(false);
+        button8.setFocusTraversable(false);
+        button9.setFocusTraversable(false);
+        button00.setFocusTraversable(false);
+        button000.setFocusTraversable(false);
+        buttonxoa.setFocusTraversable(false);
+        buttonc.setFocusTraversable(false);
+        buttonnhap.setFocusTraversable(false);
+        buttonok.setFocusTraversable(false);
+    }
+
     @FXML
     private void selectTextField(MouseEvent event) {
-        TextField clickedTextField = (TextField) event.getSource();
-        selectedTextField = clickedTextField;
+        selectedTextField = (TextField) event.getSource();
     }
     @FXML
     private void numberClick(ActionEvent event) {
-        if (selectedTextField != null) {
-            Button clickedButton = (Button) event.getSource();
-            String buttonText = clickedButton.getText();
-            selectedTextField.appendText(buttonText);
+        if (selectedTextField == null) {
+            return;
         }
+        Button clickedButton = (Button) event.getSource();
+        String buttonText = clickedButton.getText();
+        selectedTextField.appendText(buttonText);
     }
 
     @FXML
