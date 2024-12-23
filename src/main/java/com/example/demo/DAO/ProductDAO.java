dc7aa786e9c786c99b50839043f54b9fcedfbfe3
minhquang2589 <ngominhquang724@gmail.com>
2024-12-23 22:02:10 +0700

    mm

--- src/main/java/com/example/demo/DAO/ProductDAO.java
+++ src/main/java/com/example/demo/DAO/ProductDAO.java
@@ -1,10 +1,8 @@
 package com.example.demo.DAO;
 import com.example.demo.config.MySQLConnection;
 import com.example.demo.model.Product;
-
 import java.io.File;
 import java.sql.*;
-import java.util.ArrayList;
 import java.util.List;
 
 import static com.example.demo.Utils.Config.saveImage;
@@ -18,13 +16,12 @@ public class ProductDAO {
     }
 
     public void addProduct(Product product) throws SQLException {
-        String query = "INSERT INTO products (name, price, is_new, category_id, description) VALUES (?, ?, ?, ?, ?)";
+        String query = "INSERT INTO products (name, is_new, category_id, description) VALUES (?,?, ?, ?)";
         try (PreparedStatement preparedStatement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS)) {
             preparedStatement.setString(1, product.getName());
-            preparedStatement.setDouble(2, product.getPrice());
-            preparedStatement.setBoolean(3, product.isNew());
-            preparedStatement.setInt(4, product.getCategory());
-            preparedStatement.setString(5, product.getDescription());
+            preparedStatement.setBoolean(2, product.isNew());
+            preparedStatement.setInt(3, product.getCategory());
+            preparedStatement.setString(4, product.getDescription());
 
             int affectedRows = preparedStatement.executeUpdate();
 
