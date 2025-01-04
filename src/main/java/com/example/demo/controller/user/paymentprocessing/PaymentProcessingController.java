494d2aca2acae92884b624909eabb10291cdcba7
minhquang2589 <ngominhquang724@gmail.com>
2025-01-04 09:12:53 +0700

    update

--- src/main/java/com/example/demo/controller/user/paymentprocessing/PaymentProcessingController.java
+++ src/main/java/com/example/demo/controller/user/paymentprocessing/PaymentProcessingController.java
@@ -13,7 +13,6 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
 import java.io.IOException;
